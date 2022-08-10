18fd20cc3589f83297e36aa10a2ea57734e0f342
korchevIvan <unbsold@yandex.ru>
2022-08-10 22:12:04 +0300

    Setup main page, login page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    Home
+  <div class="p-12 rounded-lg bg-gray-200 text-center">
+    <h1 class="text-3xl font-medium mb-3 text-gray-800">Hi Upload 👋</h1>
+    <p class="text-gray-500">Instantly upload files, share them anyone.</p>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
-
-  }
+  components: {}
 }
 </script>
