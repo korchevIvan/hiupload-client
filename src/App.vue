aa77fbe0478130e749cfc73c7d6cb2737b6ec1ff
korchevIvan <unbsold@yandex.ru>
2022-08-11 22:21:18 +0300

    Moved header to component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
   <div class="w-full md:w-8/12 lg:w-6/12 mx-auto px-6 mt-6 md:mt-20">
-    <header class="flex flex-wrap md:flex-no-wrap items-center justify-between mb-6 -mx-3 -mt-3">
-      <ul class="flex items-center">
-        <li>
-          <router-link :to="{name: 'home'}" class="text-sm inline-block p-3 text-gray-800">Home</router-link>
-        </li>
-        <li>
-          <a href="" class="text-sm inline-block p-3 text-gray-800">Your files</a>
-        </li>
-      </ul>
-
-      <ul class="flex items-center">
-        <li>
-          <router-link :to="{name: 'login'}" class="text-sm inline-block p-3 text-gray-800">Sign In</router-link>
-        </li>
-        <li>
-          <a href="" class="text-sm inline-block p-3 text-gray-800">Create account</a>
-        </li>
-      </ul>
-    </header>
+    <app-nav />
     <router-view/>
   </div>
 </template>
+
+<script>
+import AppNav from "@/components/AppNav";
+export default {
+  components: {AppNav}
+}
+</script>
 
 <style src="./assets/styles/app.css" />
